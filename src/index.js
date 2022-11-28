59488badda90c4f48b8e4c8b14ed16433e24fdc0
Arthur <arthur.albert990@gmail.com>
2022-11-28 13:38:53 -0300

    Update functions.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 console.log('index.js working');
 
 
-export default class Battleship{
-    constructor(id,length){
+export class Battleship{
+    constructor(id = null,length){
         this.id = id;
         this.length = length;
         this.hits = 0;
@@ -14,13 +14,52 @@ export default class Battleship{
         return this.hits;
     }
     isSunk(){
+        if(this.length > this.hits){
+            this.sunk = true;
+        }
         return this.sunk;
     }
+}
+
+//Able to place ships at an x,y coord calling 
+//the Battleship factory.
+//maybe an array holding every ship?
+export class Gameboard{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.board = {
+            'coords':{x,y},
+        };
+    }
+
+    placeShip(length,x=this.x,y=this.y){
+        let ship = new Battleship (null,length);
+        this.board['coords']={x,y};
+        this.board['ship']=ship;
+    }
+    receiveAttack(x,y){
+        //receives the hit coordinates
+        //sends the hit function to the correct ship
+        // or
+        // records the coordinates of a missed shot
+    }
+
+    //keeps tracks of missed attacks.
+    missedAttacks(x,y){
+
+    }
+
 }
 
 //module.exports=Battleship;
 //CODE THE TESTS FIRST !!!!:
 // CODE THE TESTS FIRST!!!!:
+
+
+let newShip = new Gameboard(10,15);
+newShip.placeShip(5)
+console.log(newShip);
 
 // let ship = new Battleship('ship_1',10);
 // console.log(ship);
@@ -31,9 +70,17 @@ export default class Battleship{
 
 //08:35 às 09:10.
 //09:38 às 10:00.
+//08:55 às 09:48.
 
 
 // =========== ONDE PAREI ===============
+//Create Gameboard factory.
+// gameboard  está desorganizado mas funciona, ler enunciado
+// e comparar com o código.
+// dar um run-> node index.js
+
+
+// ---- later:
 //SHOULD BATTLESHIP BE A FUNCTION???
 //setup do testes, lembrar, tdd first.
 //jest não está funcionado.
